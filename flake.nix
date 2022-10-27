e5ea9654cf6613b4b4c9e36fa987dfeb12cdeefb
R-VdP <141248+R-VdP@users.noreply.github.com>
2022-10-28 00:27:07 +0200

    Use eachDefaultSystem.

--- flake.nix
+++ flake.nix
@@ -1,21 +1,19 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
 
-  outputs = { self, nixpkgs }:
-    let
-      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
-      forAllSystems = f: nixpkgs.lib.genAttrs systems f;
-    in
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system: {
+        packages = import ./pkgs {
+          pkgs = import nixpkgs { inherit system; };
+        };
+        defaultPackage = self.packages.${system}.programs-sqlite;
+      })
+    //
     {
-      packages = forAllSystems
-        (system:
-          import ./pkgs {
-            pkgs = import nixpkgs { inherit system; };
-          }
-        );
-      defaultPackage =
-        forAllSystems (system: self.packages.${system}.programs-sqlite);
-
       nixosModules.command-not-found = import ./modules/command-not-found;
       nixosModule = self.nixosModules.command-not-found;
     };
